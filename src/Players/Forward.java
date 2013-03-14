55b367fe2ce05938330dd0ea78f30fab27d70380
Peter Nycander <peter.nycander@gmail.com>
2013-03-14 14:06:38 +0100

    git ignore

--- src/Players/Forward.java
+++ src/Players/Forward.java
@@ -1,5 +1,15 @@
 package Players;
 
-public class Forward {
+import java.net.Socket;
 
+public class Forward {
+	private Socket socket;
+	
+	public static void main(String [] args) {
+		new Forward();
+	}
+	
+	public Forward() {
+		socket = new Socket();
+	}
 }
